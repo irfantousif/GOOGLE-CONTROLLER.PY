e3f17810403bdcf8df41ba7bd770e13cf5fe517b
irfantousif <68069389+irfantousif@users.noreply.github.com>
2020-07-22 13:16:14 +0530

    Add files via upload

---
+++ browser controller.py
@@ -0,0 +1,22 @@
+import webbrowser
+text=input(str("WHICH SITE WOULD U LIKE TO OPEN?:"))
+if "google" in text:
+    webbrowser.open("www.google.com")
+if "flipkart" in text:
+    webbrowser.open("www.flipkart.com")
+if "amazon" in text:
+    webbrowser.open("www.amazon.com")
+if "insta" in text:
+    webbrowser.open("www.instagram.com")
+if "facebook" in text:
+    webbrowser.open("www.facebook.com")
+if "youtube" in text:
+    webbrowser.open("www.youtube.com")
+if "gmail" in text:
+    webbrowser.open("www.gmail.com")
+if "whatsappweb" in text:
+    webbrowser.open("www.whatsappweb.com")
+if "python" in text:
+    webbrowser.open("www.python.org")
+if "github" in text:
+    webbrowser.open("www.github.com")
